34404c6b400e6b42be869b40c8621b30536ed936
Jacob Thesbjerg <jacobthesbjerg@gmail.com>
2016-08-22 13:48:35 +0200

    Initial commit

---
+++ about.php
@@ -0,0 +1,8 @@
+<?php 
+require "head.php";
+require "nav.php";
+?>
+
+    <?php
+require "footer.php";
+?>
---
+++ head.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Multimediedesigner|Jacob Thesbjerg</title>
+    <meta name="description" content="Multimediedesigner under uddannelse">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+    <header>
+        <h1>Jacob Thesbjerg</h1>
+    </header>
